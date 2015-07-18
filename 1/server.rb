ff27a048ff19e0e9ac489851ce0a4229907ebc84
mizutaki <mizutaki4@gmail.com>
2015-07-18 23:37:38 +0900

    fix #2 create thread table

--- 1/server.rb
+++ 1/server.rb
@@ -1,4 +1,19 @@
 require 'sinatra'
+require 'sequel'
+
+db = Sequel.sqlite('bbs.db')
+master = db[:sqlite_master]
+if master.where("type='table' and name='thread'").count == 0
+  db.create_table :thread do
+    primary_key :id
+    String :name
+    String :title
+    String :text
+    Date :write_date
+  end
+end
+items = db[:thread]
+items.insert(:name => 'abc',:title => 'aaaa', :text => '888', :write_date => Time.now)
 
 get '/' do
   erb :index
